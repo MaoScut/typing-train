4fcdb2a10766208e6fdd394413233044aadb8ca3
MaoScut <849345372@qq.com>
2017-12-23 12:19:59 +0800

    下落物体加上背景色

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,8 +1,10 @@
 // 小写字母
 const lowerCaseLetter = 'qwertyuiopasdfghjklzxcvbnm';
 // 大写字母
+// const upperCaseLetter = '';
 const upperCaseLetter = 'QWERTYUIOPASDFGHJKLZXCVBNM';
 // 特殊符号
+// const symbol = '';
 const symbol = '`~!@#$%^&*()_+-=|:;"<,>.?/';
 const symbol2 = '\\\'';
 const set = lowerCaseLetter + upperCaseLetter + symbol + symbol2;
