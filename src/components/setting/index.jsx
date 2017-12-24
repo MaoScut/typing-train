59b28097ada09d014d7d0f55019b2e6c49900b5c
MaoScut <849345372@qq.com>
2017-12-24 23:13:45 +0800

    设置一次训练的时间

--- src/components/setting/index.jsx
+++ src/components/setting/index.jsx
@@ -11,6 +11,7 @@ export default class Setting extends React.Component {
       fontSize: this.fontSizeInput.value,
       speed: this.speedInput.value,
       hp: this.hpInput.value,
+      time: Number(this.timeInput.value) * 60 * 1000,
     });
   }
   render() {
@@ -19,6 +20,7 @@ export default class Setting extends React.Component {
         <input defaultValue="14" ref={(input) => { this.fontSizeInput = input; }} type="text" />
         <input defaultValue="1" ref={(input) => { this.speedInput = input; }} type="text" />
         <input defaultValue="100" ref={(input) => { this.hpInput = input; }} type="text" />
+        <input defaultValue="5" ref={(input) => { this.timeInput = input; }} type="text" />
         <button onClick={this.handleClick}>开始</button>
       </div>
     );
