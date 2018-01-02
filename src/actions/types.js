673a83c09ae4d8565d3503e2dc22c8ada9cbbab5
MaoScut <849345372@qq.com>
2018-01-02 23:01:10 +0800

    over改为train over

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,4 +3,5 @@ export const SET_SPEED = 'SET_SPEED';
 export const SET = 'SET_FONT_SIZE_AND_SPEED';
 export const MISS_ONE = 'MISS_ONE';
 export const SEND_DATA = 'SEND_DATA';
-export const OVER = 'OVER';
+export const TRAIN_OVER = 'TRAIN_OVER';
+export const SUBMIT_OVER = 'SUBMIT_OVER';
