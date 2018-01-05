f46705e708911cada7e3d30aee29e9ebf937a5c8
MaoScut <849345372@qq.com>
2018-01-05 23:17:27 +0800

    一些注释，以及删除pop reducer

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,8 +3,12 @@ export const SET_SPEED = 'SET_SPEED';
 export const SET = 'SET_FONT_SIZE_AND_SPEED';
 export const MISS_ONE = 'MISS_ONE';
 export const SEND_DATA = 'SEND_DATA';
-export const TRAIN_OVER = 'TRAIN_OVER';
-export const SUBMIT_OVER = 'SUBMIT_OVER';
 export const RETRAIN = 'RETRAIN';
+
+// 切换页面
 export const ENTER_SETTING = 'ENTER_SETTING';
 export const ENTER_TRAIN = 'ENTER_TRAIN';
+
+// 一次训练完后，触发
+export const TRAIN_OVER = 'TRAIN_OVER';
+export const SUBMIT_OVER = 'SUBMIT_OVER';
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import setting from './setting';
 import data from './data';
-import pop from './pop';
 import show from './show';
 
 
